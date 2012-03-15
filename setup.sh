ab073566aa76454fa168b6d40bf254dfa18ec8dc
Alexander Rust <mail@alr.st>
2012-03-15 12:43:24 +0100

    Fixed setup script creation of symlinks inside symlink targets, adapted ta script, added temperatures to tmux statusbar

--- setup.sh
+++ setup.sh
@@ -2,22 +2,22 @@
 
 SCRIPTPATH="$(cd "${0%/*}" 2>/dev/null; echo "$PWD"/"${0##*/}")"
 BASEDIR=`dirname "$SCRIPTPATH"`
-BACKUPDIR=$BASEDIR/backup
 
-function doIt() { 
-  ln -f -s $BASEDIR/vim ~/.vim 
-  ln -f -s $BASEDIR/zsh ~/.zsh
-  ln -f -s $BASEDIR/bash ~/.bash
+function doIt() {  
+  # Note: -T prevents creatin of symlink in existing symlinks target, thus replaces the link for folders
+  ln -f -s -T $BASEDIR/vim ~/.vim
+  ln -f -s -T $BASEDIR/zsh ~/.zsh
+  ln -f -s -T $BASEDIR/bash ~/.bash
   ln -f -s $BASEDIR/zsh/zshrc ~/.zshrc
   ln -f -s $BASEDIR/bash/bashrc ~/.bashrc
   ln -f -s $BASEDIR/tmux/tmux.conf ~/.tmux.conf
-  ln -f -s $BASEDIR/scripts ~/.scripts
+  ln -f -s -T $BASEDIR/scripts ~/.scripts
   ln -f -s $BASEDIR/git/gitignore ~/.gitignore
   ln -f -s $BASEDIR/gem/gemrc ~/.gemrc
 
   # Protect user gem dir from accidential writing
   if [ -d "~/.gem" ]; then
-	mod -R u-w ~/.gem
+	chmod -R u-w ~/.gem
   fi
 
   if ! type -p git > /dev/null; then

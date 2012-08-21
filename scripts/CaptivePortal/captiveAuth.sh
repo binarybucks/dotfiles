ef09c0b2ccc2982adb604ffa2fbd05613d3e66a5
Alexander Rust <mail@alr.st>
2012-08-21 21:03:47 +0200

    Some cleanup

--- scripts/CaptivePortal/captiveAuth.sh
+++ scripts/CaptivePortal/captiveAuth.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-
+# Auths user with username and password at university firewall 
 cd /tmp
 /opt/local/bin/wget https://nacs.dhbw-stuttgart.de/login-exec.php --no-check-certificate --keep-session-cookies --save-cookies login.cookie -O - > /dev/null
 /opt/local/bin/wget https://nacs.dhbw-stuttgart.de/login-exec.php --no-check-certificate --post-data "username=$DHUSERNAME&password=$DHPW" --load-cookies login.cookie -O - > /dev/null 
